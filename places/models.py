4e63591899b8eb79d3b24998af5683646c2d4bef
IlyaG96 <ilyag_96@icloud.com>
2022-01-07 21:25:42 +1100

    add Post model

--- places/models.py
+++ places/models.py
@@ -1,2 +1,16 @@
 from django.db import models
 # Create your models here.
+
+
+class Post(models.Model):
+    title = models.CharField(max_length=256)
+    images = models.JSONField()
+    description_short = models.TextField()
+    description_long = models.TextField()
+    coordinates = models.JSONField()
+
+    def __str__(self):
+        """
+        String for representing the MyModelName object (in Admin site etc.)
+        """
+        return self.title
