d5bdf3fc81a2ae25ef2519231a554a226260b6ae
IlyaG96 <ilyag_96@icloud.com>
2022-01-11 17:42:26 +1100

    add preview images in /admin

--- places/admin.py
+++ places/admin.py
@@ -2,14 +2,21 @@ from django.contrib import admin
 from .models import Post
 from .models import Image
 # Register your models here.
+from django.utils.html import format_html
 
 
-class PostImageAdmin(admin.StackedInline):
+class PostImageAdmin(admin.TabularInline):
+
     model = Image
+    readonly_fields = "image_preview",
+
+    def image_preview(self, image):
+        return format_html(f'<img src="{image.image.url}" width="200"')
 
 
 @admin.register(Post)
 class AuthorAdmin(admin.ModelAdmin):
+
     inlines = [
         PostImageAdmin
     ]
@@ -21,3 +28,4 @@ class AuthorAdmin(admin.ModelAdmin):
 @admin.register(Image)
 class PostImageAdmin(admin.ModelAdmin):
     pass
+
