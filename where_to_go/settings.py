036ea1d36ba90d6d6c8288ca08ba55b96addeaf5
IlyaG96 <ilyag_96@icloud.com>
2022-01-16 20:23:12 +1100

    add .env configurations

--- where_to_go/settings.py
+++ where_to_go/settings.py
@@ -12,24 +12,14 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 
 from pathlib import Path
 import os
-
-# Build paths inside the project like this: BASE_DIR / 'subdir'.
+from environs import Env
 BASE_DIR = Path(__file__).resolve().parent.parent
+env = Env()
 
+ALLOWED_HOSTS = env.list('ALLOWED_HOSTS', [])
+SECRET_KEY = env.str('SECRET_KEY', 'REPLACE_ME')
+DEBUG = env.bool('DEBUG', True)
 
-# Quick-start development settings - unsuitable for production
-# See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
-
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-rq$l%qdcl4^a&y#9!wrk0)1682&%k78z63z!#$oj^iplfbzy54'
-
-# SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
-
-ALLOWED_HOSTS = []
-
-
-# Application definition
 
 INSTALLED_APPS = [
     'django.contrib.admin',
@@ -74,9 +64,6 @@ TEMPLATES = [
 WSGI_APPLICATION = 'where_to_go.wsgi.application'
 
 
-# Database
-# https://docs.djangoproject.com/en/4.0/ref/settings/#databases
-
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
@@ -84,9 +71,6 @@ DATABASES = {
     }
 }
 
-
-# Password validation
-# https://docs.djangoproject.com/en/4.0/ref/settings/#auth-password-validators
 
 AUTH_PASSWORD_VALIDATORS = [
     {
@@ -104,9 +88,6 @@ AUTH_PASSWORD_VALIDATORS = [
 ]
 
 
-# Internationalization
-# https://docs.djangoproject.com/en/4.0/topics/i18n/
-
 LANGUAGE_CODE = 'en-us'
 
 TIME_ZONE = 'UTC'
@@ -116,9 +97,6 @@ USE_I18N = True
 USE_TZ = True
 
 
-# Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/4.0/howto/static-files/
-
 STATIC_URL = 'static/'
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, "static")
@@ -127,10 +105,9 @@ MEDIA_URL = 'media/'
 MEDIA_DIRS = [
     os.path.join(BASE_DIR, "media")
 ]
-# Default primary key field type
-# https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
 TINYMCE_DEFAULT_CONFIG = {
     "theme": "silver",
     "height": 300,
