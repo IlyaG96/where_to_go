a1eae68594e140f3f151a2ed9c34d1578d31c882
IlyaG96 <ilyag_96@icloud.com>
2022-02-03 21:13:50 +1100

    allowed_hosts, secret_key, debug from .env

--- where_to_go/settings.py
+++ where_to_go/settings.py
@@ -16,10 +16,10 @@ import os
 from environs import Env
 BASE_DIR = Path(__file__).resolve().parent.parent
 env = Env()
-
+env.read_env()
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "where_to_go.settings")
 
-ALLOWED_HOSTS = env.list('ALLOWED_HOSTS', [])
+ALLOWED_HOSTS = env.list('ALLOWED_HOSTS', '127.0.0.1')
 SECRET_KEY = env.str('SECRET_KEY', 'REPLACE_ME')
 DEBUG = env.bool('DEBUG', True)
 
@@ -117,6 +117,7 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 INTERNAL_IPS = [
     "127.0.0.1",
 ]
+
 TINYMCE_DEFAULT_CONFIG = {
     'theme': 'silver',
     'height': 300,
