ffc13c89c3d173a5710cc680070bcf72ecd86e3e
IlyaG96 <ilyag_96@icloud.com>
2022-02-04 18:32:00 +1100

    fix ensure_ascii

--- places/views.py
+++ places/views.py
@@ -58,4 +58,4 @@ def places(request, post_id):
     }
     return JsonResponse(response_data,
                         safe=False,
-                        json_dumps_params={"ensure_ascii": False, "indent": 2})
+                        json_dumps_params={"ensure_ascii": True, "indent": 2})
