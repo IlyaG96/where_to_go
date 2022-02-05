513be35004bfdbbec382491def6c1d813f149452
IlyaG96 <ilyag_96@icloud.com>
2022-02-05 16:54:38 +1100

    use mark_safe and format html instead of f-strings

--- places/admin.py
+++ places/admin.py
@@ -1,22 +1,23 @@
 from django.contrib import admin
 from .models import Point
 from .models import Image
-from django.utils.html import format_html
+from django.utils.html import format_html, mark_safe
 from adminsortable2.admin import SortableInlineAdminMixin
 
 
 class PostImageAdmin(SortableInlineAdminMixin, admin.StackedInline):
-
     model = Image
     readonly_fields = "image_preview",
 
     def image_preview(self, image):
-        return format_html(f"<img src='{image.image.url}' width='150'")
+
+        response = format_html("<img src={} width=150>", mark_safe(image.image.url))
+
+        return response
 
 
 @admin.register(Point)
 class AuthorAdmin(admin.ModelAdmin):
-
     search_fields = ['title']
 
     inlines = [
