25ccb9aa9ef3aad09323d17f70ba1ebae62e08a5
IlyaG96 <ilyag_96@icloud.com>
2022-02-05 22:17:01 +1100

    use get_object_or_404

--- places/views.py
+++ places/views.py
@@ -1,9 +1,8 @@
 from django.shortcuts import render
 from places.models import Point
 from django.http import JsonResponse
-from django.http import HttpResponse
-from django.utils.html import format_html, mark_safe
 from django.urls import reverse_lazy
+from django.shortcuts import get_object_or_404
 
 
 def serialize_content(point):
@@ -31,19 +30,7 @@ def index_page(request):
 
 
 def places(request, point_id):
-    try:
-        current_point = Point.objects.get(id=point_id)
-    except Point.DoesNotExist:
-
-        content = ('<html>'
-                   '<body>'
-                   'Такого места еще не существует'
-                   '<p><a href = "/">На главную</a></p>'
-                   '</body>'
-                   '</html>')
-
-        response = format_html(mark_safe(content))
-        return HttpResponse(response)
+    current_point = get_object_or_404(Point, id=point_id)
 
     title = current_point.title
     description_short = current_point.description_short
